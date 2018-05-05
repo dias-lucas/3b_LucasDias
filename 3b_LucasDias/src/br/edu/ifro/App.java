cbcef934c5a90edc850bebd43cde9511e3482c1e
lucasdias77 <lucasdias.ifro@gmail.com>
2018-05-05 00:44:47 -0300

    instalando o hibernate

--- 3b_LucasDias/src/br/edu/ifro/App.java
+++ 3b_LucasDias/src/br/edu/ifro/App.java
@@ -19,18 +19,14 @@ import javafx.stage.Stage;
 public class App extends Application {
     
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("CadastroCliente.fxml"));
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
